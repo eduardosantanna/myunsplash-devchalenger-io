9967195302c8c70916d9d6d95604d3df15813a87
Luis Eduardo <luiseduess@gmail.com>
2023-04-25 23:59:03 -0300

    feat: config for react-query

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,7 +3,14 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
