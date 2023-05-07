e25de308dfaade3adc2437cc4a0ac8ebda0a5b82
Luis Eduardo <luiseduess@gmail.com>
2023-05-07 20:03:13 -0300

    refactor: image validation pipe tweaks

--- backend/src/app/image-save-url/validation-pipes/image-url-validate.pipe.ts
+++ backend/src/app/image-save-url/validation-pipes/image-url-validate.pipe.ts
@@ -10,29 +10,33 @@ import { CreateImageDto } from '../dto/create-image.dto'
 @Injectable()
 export class ImageUrlValidatePipe implements PipeTransform {
   async transform(imageContent: CreateImageDto) {
-    try {
-      const { data, headers } = await axios.get(imageContent.imageUrl, {
-        responseType: 'arraybuffer',
-      })
-      const mimeType = headers['content-type'] as string
+    const { data, headers } = await axios.get(imageContent.imageUrl, {
+      responseType: 'arraybuffer',
+    })
+    const mimeType = headers['content-type'] as string
+    const imageSize = +data.byteLength / 1000
 
-      if (!mimeType.match(/image\/(png|jpeg)$/)) {
-        throw new HttpException(
-          'Entered URL is not a PNG or JPEG type image.',
-          HttpStatus.BAD_REQUEST
-        )
-      }
-
-      imageContent.imageBuffer = data
-      imageContent.imageContentType = mimeType
-
-      return imageContent
-    } catch (error) {
-      console.log(error)
+    if (!mimeType.match(/image\/(png|jpeg)$/)) {
       throw new HttpException(
-        { message: ['Entered URL is not a PNG or JPEG type image.'] },
+        {
+          message: ['Entered URL is not a PNG or JPEG type image.'],
+        },
         HttpStatus.BAD_REQUEST
       )
     }
+
+    if (imageSize > 2548) {
+      throw new HttpException(
+        {
+          message: ['Your image must be a maximum of 2Mb.'],
+        },
+        HttpStatus.BAD_REQUEST
+      )
+    }
+
+    imageContent.imageBuffer = data
+    imageContent.imageContentType = mimeType
+
+    return imageContent
   }
 }
